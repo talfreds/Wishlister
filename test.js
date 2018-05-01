defb8071633d9e63e00b52a6c0fd0e3ec2502158
robertjanzen <robertjanzenbc@gmail.com>
2018-05-01 16:20:30 -0700

    Add sections for sql db tests

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 var steam = require("./steam")
+var sql = require("./sql_db.js")
 
 test("a valid object", ()=>{
   expect(process_object()).
   toBe()
+});
+
+describe('SQL DB Tests', () => {
+  test("Connect to RDS MySQL database", () => {
+  });
 });
