7f35db7f9224c1e667d7df127d782d1b12033da9
Quinton0121 <quinton0121@gmail.com>
2018-05-01 17:03:43 -0700

    add animation to login window

--- public/client.js
+++ public/client.js
@@ -3,8 +3,10 @@ var logWindow = document.getElementById('logWindow');
 
 document.getElementById('login-header').addEventListener('click',function(){
   logWindow.style.display = 'block';
+  document.getElementById('blur').style.filter = 'blur(5px)';
 });
 
 document.getElementById('logIn-close').addEventListener('click',function(){
   logWindow.style.display = 'none';
+  document.getElementById('blur').style.filter = 'blur(0px)';
 });
