ec616f304487c791c268b20ac3897dc7127b9426
EXcurrybat <32018466+EXcurrybat@users.noreply.github.com>
2018-05-08 16:00:06 -0700

    Tests for gog.js (#44)
    * a
    
    * basic gog unittest
    
    * test 2 for returning a single game as object

--- gog.js
+++ gog.js
@@ -16,15 +16,24 @@ var gog_api = (game_name) => {
         } else {
 
             var gameList = body.products;
-
             resolve(gameList);
           }
         });
   });
 }
 
+var isolate_game_obj = (game_name, game_list) => {
+  var game_obj = undefined;
+  for(i in game_list){
+    if(game_list[i].title == game_name){
+      game_obj = game_list[i];
+    }
+  }
+
+  return game_obj
+}
 
 
  module.exports = {
-   gog_api
+   gog_api, isolate_game_obj
  }
